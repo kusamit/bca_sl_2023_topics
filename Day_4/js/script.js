4f154647caf4405cbb249c02edf55b17aceaff80
kusamit <amitkus012@gmail.com>
2023-12-06 10:55:17 +0545

    Day_4 2nd Commit

--- Day_4/js/script.js
+++ Day_4/js/script.js
@@ -1,15 +1,15 @@
 //defining array
 let palces=["kathmandu","lalitpur","pokhara","Butwal"]
 const gender=["Male","Female","Others"]
-
+console.log("View of Position of Array 1")
 //accessing array elements with indexing
 console.log(palces[0])
 console.log(palces[1])
-console.log(palces[1])
+console.log(palces[2])
 
 //since array is an object so we can define it using array()
 let lakes= new Array("Phewa","Begnas","Rara","Tillicho")
-
+console.log("view of Position Array 2")
 //accessing array elements with indexing
 console.log(lakes[0])
 console.log(lakes[1])
@@ -19,6 +19,7 @@ console.log(lakes[1])
 //properties
 //length - return the length of an array
 //example
+console.log("Length of Array Example")
 console.log(palces.length)
 console.log(gender.lenght)
 console.log(lakes.length)
@@ -26,7 +27,7 @@ console.log(lakes.length)
 //methods
 //1. push - append an element in an array
 //example
-
+console.log("Push Example")
 let days=["Sunday","Monday"]
 console.log(days)
 days.push("Tuesday")
@@ -35,6 +36,7 @@ days.push("Wednesday")
 console.log(days)
 
 //2. pop -removes last index element from an array
+console.log("Pop Example")
 let capital=["Kathmandu","New Delhi","Beijing","Dhaka"]
 console.log(capital)
 capital.pop()
